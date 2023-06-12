be98c08414b4ad706f1975f00fe92d3a23a4bff6
Sandrieli13 <tskitishvilisandro@gmail.com>
2023-06-11 21:14:58 -0400

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,32 +1,66 @@
-const express = require('express');
-const path = require('path');
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
-// Middleware
-app.use(express.json());
-
 // API routes
-const usersRouter = require('./routes/users');
-const productsRouter = require('./routes/products');
+const usersRouter = require('./api/users');
+const productsRouter = require('./api/products');
 
 app.use('/api/users', usersRouter);
 app.use('/api/products', productsRouter);
 
-// Serve index.html
-app.use(express.static('public'));
+app.get("/config", (req, res) => {
+  res.send({
+    publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  });
+});
+
+// Logging middleware
+app.use(morgan("dev"));
+
+// Body parsing middleware
+app.use(express.json());
+
+// Add a custom middleware to log incoming requests
+app.use((req, res, next) => {
+  console.log(`Incoming request: ${req.method} ${req.url}`);
+  next();
+});
+
+// Auth and API routes
+app.use("/auth", require("./auth"));
+app.use("/api", require("./api"));
+app.use("/create-payment-intent", require("./api/payment"));
+
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "..", "public/index.html"))
+);
+
+// Static file-serving middleware
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+// Handle requests with an extension (.js, .css, etc.) - send 404
+app.use((req, res, next) => {
+  if (path.extname(req.path).length) {
+    const err = new Error("Not found");
+    err.status = 404;
+    next(err);
+  } else {
+    next();
+  }
+});
 
 // Error handling middleware
 app.use((err, req, res, next) => {
-  console.error(err); // Log the error for debugging purposes
-
-  // Provide a detailed error message in the response
-  res.status(500).json({ error: 'Internal server error' });
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || "Internal server error.");
 });
 
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+// Send index.html for any remaining requests
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public/index.html"));
 });
 
 module.exports = app;
